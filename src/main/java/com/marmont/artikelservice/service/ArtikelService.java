97af5390d506ec968261f43403e5bc2f5cbb8720
Dietmar Darimont <78258399+M4rm0nt@users.noreply.github.com>
2023-06-05 03:34:19 +0200

    Update ArtikelService.java
    Implement optional, @Data for HashCode, Equals and toString.

--- src/main/java/com/marmont/artikelservice/service/ArtikelService.java
+++ src/main/java/com/marmont/artikelservice/service/ArtikelService.java
@@ -6,35 +6,40 @@ import com.marmont.artikelservice.exceptions.*;
 import com.marmont.artikelservice.mapper.ArtikelMapper;
 import com.marmont.artikelservice.model.Artikel;
 import com.marmont.artikelservice.repository.ArtikelRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Data
 public class ArtikelService {
 
     private final ArtikelRepository artikelRepository;
-    
+
     private static final int DELETED = -1;
     private static final int INCREMENT_BY_ONE = 1;
 
     public ArtikelResponse createArtikel(ArtikelRequest artikelRequest) {
-        
+
         var artikel = ArtikelMapper.INSTANCE.mapToArtikel(artikelRequest);
         var savedArtikel = artikelRepository.save(artikel);
+
         log.info("Artikel with id: {} is saved!", savedArtikel.getId());
-        
+
         return ArtikelMapper.INSTANCE.mapToArtikelResponse(savedArtikel);
     }
 
     public List<ArtikelResponse> getAllArtikel() {
-        
+
         var listOfArtikel = artikelRepository.findByStatusNot(DELETED);
+
         log.info("All Artikel found!");
 
         return listOfArtikel.stream()
@@ -43,21 +48,21 @@ public class ArtikelService {
     }
 
     public ArtikelResponse getArtikelById(long id) {
-        
+
         var artikel = artikelRepository.findById(id)
                 .orElseThrow(() -> new ArtikelNotFoundException("Artikel not found with id: " + id));
-        
+
         if (artikel.getStatus() == DELETED) {
             throw new ArtikelNotFoundException("Artikel not found with id: " + id);
         }
-        
+
         log.info("Artikel found with id: {}", artikel.getId());
 
         return ArtikelMapper.INSTANCE.mapToArtikelResponse(artikel);
     }
 
     public ArtikelResponse updateArtikel(long id, ArtikelRequest artikelRequest) {
-        
+
         var existingArtikel = artikelRepository.findById(id)
                 .orElseThrow(() -> new ArtikelNotFoundException("Artikel not found with id: " + id));
 
@@ -66,7 +71,9 @@ public class ArtikelService {
         existingArtikel.setEinkaufspreis(artikelRequest.getEinkaufspreis());
         existingArtikel.setVerkaufspreis(artikelRequest.getVerkaufspreis());
         existingArtikel.setVersion(existingArtikel.getVersion() + INCREMENT_BY_ONE);
-        Artikel updatedArtikel = artikelRepository.save(existingArtikel);
+
+        var updatedArtikel = artikelRepository.save(existingArtikel);
+
         log.info("Artikel with id: {} is updated!", updatedArtikel.getId());
 
         return ArtikelMapper.INSTANCE.mapToArtikelResponse(updatedArtikel);
@@ -74,10 +81,11 @@ public class ArtikelService {
 
     public void markAsDeleted(long id) {
 
-        var artikel = artikelRepository.findById(id).orElseThrow(() -> new ArtikelNotFoundException("Artikel not found with id: " + id));
-        artikel.setStatus(DELETED);
-        artikelRepository.save(artikel);
-        log.info("Artikel with id: {} is deleted!", id);
+        artikelRepository.findById(id).ifPresent(artikel -> {
+            artikel.setStatus(DELETED);
+            artikelRepository.save(artikel);
+            log.info("Artikel with id: {} is deleted!", id);
+        });
     }
 
     public List<ArtikelResponse> searchArtikel(String beschreibung, String ean) {
